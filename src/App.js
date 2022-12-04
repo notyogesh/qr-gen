877c19bdb8c2e105fda4089bf58aee2192e584e0
notyogesh <yogeshreo@gmail.com>
2022-12-04 12:21:26 +0530

    need media query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from "./logo.svg";
+import QRCode from "qrcode";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(
+      url,
+      {
+        Width: 800,
+      },
+      (err, url) => {
+        if (err) return console.error(err);
+        console.log(url);
+        setQrCode(url);
+      }
+    );
+  };
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className="top">
+        <input
+          type="text"
+          placeholder="input here..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button onClick={GenerateQRCode}>Generate</button>
+      </div>
+      {qrCode && (
+        <div className="bot">
+          <img src={qrCode} alt="code" />
+          <a href={qrCode} download="qrCode.png">
+            Download
+          </a>
+        </div>
+      )}
     </div>
   );
 }
